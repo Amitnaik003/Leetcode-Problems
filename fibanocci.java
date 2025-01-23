3cba723ff6bef90891ee2da724f0c62837e752a9
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-01-23 02:48:50 +0000

    Add files via upload

---
+++ fibanocci.java
@@ -0,0 +1,11 @@
+public class fibanocci {
+        public int fib(int n) {
+            int a = 0, b = 1;
+            while (n-- > 0) {
+                int c = a + b;
+                a = b;
+                b = c;
+            }
+            return a;
+        }
+    }
