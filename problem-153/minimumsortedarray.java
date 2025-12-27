7a88253cd8eb9150fcc371c330eb5a5c2be28de5
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-12-27 10:03:23 +0530

    Update minimumsortedarray.java

--- problem-153/minimumsortedarray.java
+++ problem-153/minimumsortedarray.java
@@ -5,7 +5,7 @@ public class minimimsotedarray {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left < right) { 
+        while (left < right) {  
             int mid = left + (right - left) / 2; 
 
             if (nums[mid] > nums[right]) {
