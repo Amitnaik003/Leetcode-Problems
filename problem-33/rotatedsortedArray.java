43566518e15bccb9ff51129e767ab81366b709b2
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2026-01-15 21:41:43 +0530

    Update rotatedsortedArray.java

--- problem-33/rotatedsortedArray.java
+++ problem-33/rotatedsortedArray.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class rotatedsortedArray{
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
-
+  
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
